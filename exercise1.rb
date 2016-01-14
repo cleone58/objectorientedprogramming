7548ba0c2d4e6caa28643d253f6ac1a043543493
Cleone Jacob <therealcleonejacob@gmail.com>
2016-01-14 14:40:20 -0500

    instructor_student

---
+++ exercise1.rb
@@ -0,0 +1,40 @@
+
+class Person
+attr_accessor :name
+def initialize(name)
+    @name = name
+end
+
+def greeting
+  puts "Hi, my name is #{@name}"
+
+end
+
+end
+
+class Student <Person
+
+def learn
+puts "I get it!"
+end
+
+end
+
+class Instructor <Person
+
+def teach
+puts "Everything in Ruby is Object"
+
+end
+
+end
+
+chris = Instructor.new ("chris")
+
+chris.name = 'chris'
+
+christina = Student.new("christina")
+christina.name = "christina"
+
+chris.teach
+christina.learn
